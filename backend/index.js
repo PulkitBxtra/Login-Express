a55e285881273c4f819edee6e465442bf597bb74
PulkitBxtra <PulkitBxtra@users.noreply.github.com>
2023-03-23 02:02:42 +0530

    finished backend

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const cors=require('cors');
+const connection = require('./db');
+const userRoutes = require('./Routes/users');
+const authRoutes = require('./Routes/auth');
+
+
+//database connection
+connection()
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+//routes
+app.use('/api/users',userRoutes);
+app.use('/api/auth',authRoutes);
+
+const port=process.env.PORT ||8000;
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`);
+})
