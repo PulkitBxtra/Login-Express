8a138a7e501fb4c049321ca9951887a25f414b64
PulkitBxtra <PulkitBxtra@users.noreply.github.com>
2023-03-24 15:19:42 +0530

    fixed db.js

--- backend/db.js
+++ backend/db.js
@@ -9,8 +9,8 @@ module.exports= () =>{
     };
 
     try{
-        // mongoose.connect(process.env.DB,connectionParams);
-        mongoose.connect("mongodb+srv://Pulkit:Batra123b@mym-task-db.m1ckoyl.mongodb.net/?retryWrites=true&w=majority",connectionParams);
+        mongoose.connect(process.env.DB,connectionParams);
+        // mongoose.connect("mongodb+srv://Pulkit:Batra123b@mym-task-db.m1ckoyl.mongodb.net/?retryWrites=true&w=majority",connectionParams);
 
         console.log(`connected to Database`);
     }
