2af8a7c140a3d4cd1bebd408ea3a0d18c2bc834a
PulkitBxtra <PulkitBxtra@users.noreply.github.com>
2023-03-24 15:04:12 +0530

    fixed user.js

--- frontend/src/Components/Signup/index.jsx
+++ frontend/src/Components/Signup/index.jsx
@@ -20,8 +20,8 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const url = "http://localhost:8000/api/users";
-      // const url = "https://mym-backend.onrender.com/api/users";
+      // const url = "http://localhost:8000/api/users";
+      const url = "https://mym-backend.onrender.com/api/users";
       const { data: res } = await axios.post(url, data);
       navigate("/login");
       console.log(res.message);
