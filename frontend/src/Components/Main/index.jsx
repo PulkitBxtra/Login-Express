9d3acbfe65e2ed2e1c47291b720d583447984855
PulkitBxtra <PulkitBxtra@users.noreply.github.com>
2023-03-24 23:56:05 +0530

    fixed env variables

--- frontend/src/Components/Main/index.jsx
+++ frontend/src/Components/Main/index.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import styles from "./styles.module.css";
 
 const Main = () => {
@@ -5,6 +6,21 @@ const Main = () => {
 		localStorage.removeItem("token");
 		window.location.reload();
 	};
+
+	const [imageData, setImageData] = useState(null);
+
+	useEffect(() => {
+	  fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+		.then(response => response.json())
+		.then(data => setImageData(data))
+		.catch(error => console.error(error));
+	}, []);
+  
+	if (!imageData) {
+	  return <p>Loading...</p>;
+	}
+  
+	const { url, title, explanation } = imageData;
 
 	return (
 		<div className={styles.main_container}>
@@ -14,6 +30,12 @@ const Main = () => {
 					Logout
 				</button>
 			</nav>
+
+			<div className="NasaImage"> {/* Add a class name */}
+				<h2>{title}</h2>
+				<img src={url} alt={title} />
+			<p>{explanation}</p>
+    </div>
 		</div>
 	);
 };
